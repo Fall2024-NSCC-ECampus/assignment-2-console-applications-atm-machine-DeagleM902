49b24cc78d8d047ba1dfe4d93709c055d20fa9fb
DeagleM <W0258876@campus.nscc.ca>
2024-10-24 22:31:14 -0300

    Initial file, check written notes

---
+++ ATMMachine.cpp
@@ -0,0 +1,50 @@
+//
+// Created by mattd on 2024-10-24.
+//
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void printIntroMenu();
+void printMainMenu();
+void start();
+void login();
+void createAccount();
+
+// global variable (use this variable to store the user’s menu selection)
+char menuInput;
+
+// the main function
+int main()
+{
+    // TO WRITE A WELCOME MESSAGE HERE
+    // call the function start
+    start();
+    return 0;
+}
+
+void printIntroMenu()
+{
+    // WRITE CODE HERE
+}
+
+void printMainMenu()
+{
+    // WRITE CODE HERE
+}
+
+void start()
+{
+    // EXPLANATION OF CODE THAT GOES HERE IS BELOW
+}
+
+void createAccount()
+{
+    // PHASE 2
+}
+
+void login()
+{
+    // PHASE 2
+}
