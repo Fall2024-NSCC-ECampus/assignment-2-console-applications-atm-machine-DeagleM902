7200b2ee20eef75182be821fb4fbfcdf6429f57f
DeagleM <W0258876@campus.nscc.ca>
2024-10-25 16:32:28 -0300

    Menu prints, first switch/case

--- ATMMachine.cpp
+++ ATMMachine.cpp
@@ -3,7 +3,7 @@
 //
 
 #include <iostream>
-#include <stdlib.h>
+#include <map>
 using namespace std;
 
 void printIntroMenu();
@@ -12,39 +12,74 @@ void start();
 void login();
 void createAccount();
 
-// global variable (use this variable to store the user’s menu selection)
+//Global variables
 char menuInput;
+map<string, string> accounts;
+double balance;
+bool isLoggedIn = false;
+string currentUser;
 
-// the main function
-int main()
-{
-    // TO WRITE A WELCOME MESSAGE HERE
-    // call the function start
+//Main function, header files will be used for part B
+int main() {
+    cout << "ATM machine start." << endl;
     start();
     return 0;
 }
 
-void printIntroMenu()
-{
-    // WRITE CODE HERE
+void printIntroMenu() {
+    cout << "Please select an option from the menu below:" << endl;
+    cout << "l -> Login" << endl;
+    cout << "c -> Create New Account" << endl;
+    cout << "q -> Quit" << endl;
+    cout << "> ";
 }
 
-void printMainMenu()
-{
-    // WRITE CODE HERE
+void printMainMenu() {
+    cout << "Please select an option from the menu below:" << endl;
+    cout << "d -> Deposit Money" << endl;
+    cout << "w -> Withdraw Money" << endl;
+    cout << "r -> Request Balance" << endl;
+    cout << "l -> Logout" << endl;
+    cout << "q -> Logout and Quit" << endl;
+    cout << "> ";
 }
 
-void start()
-{
-    // EXPLANATION OF CODE THAT GOES HERE IS BELOW
+//Logic to handle interaction
+void start() {
+    do {
+        printIntroMenu();
+        cin >> menuInput;
+        switch (menuInput) {
+            case 'l':
+                login();
+                break;
+            case 'c':
+                createAccount();
+                break;
+            case 'q':
+                cout << "Goodbye!" << endl;
+                exit(0);
+            default:
+                cout << "Invalid option. Please try again." << endl;
+        }
+    } while (menuInput != 'q');
 }
 
-void createAccount()
-{
-    // PHASE 2
+//Ideally, there should be a way to back out of account creation
+//TODO - PART B - "Database" file to read/write accounts
+void createAccount() {
+    string id, password;
+    cout << "Please create your user id:" << endl;
+    cin >> id;
+    cout << "Please enter your PIN:" << endl;
+    cin >> password;
+
+    accounts[id] = password;
+    cout << "Account successfully created." << endl;
 }
 
-void login()
-{
+//Handles login, actions once logged in
+//TODO - PART B - Move logic to align with OOP, read file for login
+void login() {
     // PHASE 2
 }
