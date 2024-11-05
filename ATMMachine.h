81589ccb44066218e75dac3edc157d44d636ff67
DeagleM <W0258876@campus.nscc.ca>
2024-11-05 15:13:19 -0400

    menu logic header

--- ATMMachine.h
+++ ATMMachine.h
@@ -4,11 +4,27 @@
 
 #ifndef ATMMACHINE_H
 #define ATMMACHINE_H
+#include "AccountManager.h"
 
-
+using namespace std;
 
 class ATMMachine {
+public:
+    ATMMachine();
+    void start();
 
+private:
+    void printIntroMenu();
+    void printMainMenu();
+    void login();
+    void createAccount();
+    void deposit(double amount);
+    void withdraw(double amount);
+    void checkBalance();
+
+    bool isLoggedIn;
+    string currentUser;
+    AccountManager accountManager;
 };
 
 
